5ff590d92f1892673df1a62b6a7883edd0b1d1aa
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2026-09-14 22:10:51 -0300

    criando as rotas

--- src/server.js
+++ src/server.js
@@ -5,6 +5,17 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  // Early return
+  if (method === "GET" && url === "/users") {
+    return res.end("Listagem de usuarios");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return res.end("Criação de usuarios");
+  }
+
   return res.end("Hellooooooooooooo");
 });
 
